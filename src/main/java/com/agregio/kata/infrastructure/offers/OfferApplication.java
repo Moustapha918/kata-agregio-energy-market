be1a082bf299d45d9070987d34f34df5f2d04e4d
moustaphakaber <kabermoustapha@gmail.com>
2023-11-27 19:06:46 +0100

    :test_tube: add feature : get offers by market

--- src/main/java/com/agregio/kata/infrastructure/offers/OfferApplication.java
+++ src/main/java/com/agregio/kata/infrastructure/offers/OfferApplication.java
@@ -1,10 +1,14 @@
 package com.agregio.kata.infrastructure.offers;
 
 
+import com.agregio.kata.domain.market.EnumMarketType;
 import com.agregio.kata.domain.offers.Offer;
 import com.agregio.kata.domain.usecases.OffersAPI;
 import com.agregio.kata.domain.usecases.PwoFunctionalException;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class OfferApplication {
@@ -19,5 +23,11 @@ public class OfferApplication {
         return this.offersAPI.createOffer(offer)
                 .map(OfferDTO::fromDomain)
                 .orElseThrow();
+    }
+
+    public List<OfferDTO> loadOffersByMarket(EnumMarketType market) {
+        return offersAPI.loadOffersByMarket(market)
+                .stream().map(OfferDTO::fromDomain)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/agregio/kata/infrastructure/offers/OffersController.java
+++ src/main/java/com/agregio/kata/infrastructure/offers/OffersController.java
@@ -1,11 +1,12 @@
 package com.agregio.kata.infrastructure.offers;
 
+import com.agregio.kata.domain.market.EnumMarketType;
 import com.agregio.kata.domain.usecases.PwoFunctionalException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.agregio.kata.infrastructure.WebConstants.API_ROOT_URL;
 
@@ -23,5 +24,11 @@ public class OffersController {
     public ResponseEntity<OfferDTO> createOffer(@RequestBody OfferCreationDTO offerToCreate) throws PwoFunctionalException {
         var  createdOffer = offerApplication.createOffer(offerToCreate);
         return ResponseEntity.ok(createdOffer);
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE}, value = API_ROOT_URL + "/offers/{market}")
+    public ResponseEntity<List<OfferDTO>> loadOffersByMarket(@PathVariable EnumMarketType market){
+        var  offers = offerApplication.loadOffersByMarket(market);
+        return ResponseEntity.ok(offers);
     }
 }
